a8d4c71368f7f0b0d0c840ec61042926c533c1d5
Jack Cui <jackcuikq@gmail.com>
2020-06-27 19:23:52 -0400

    added login and register pages

--- GuitarToTab.py
+++ GuitarToTab.py
@@ -1,7 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8876cbfb71d8a36eff20965d7d04dde1'
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return "<h1>GuitarToTab</h1>"
+    return render_template('home.html', title='Home')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@guitar.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')        
+            return redirect(url_for('home'))
+
+    return render_template('login.html', title='Login', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
